a18572bdb200ec722f9bb0d31b61a939bea3f55f
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-11-08 00:57:51 +0530

    creating a student class and constructor

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student{
+    private String name;
+    private int id;
+    private String course;
+    private String email;
+
+    public Student(String name, int id, String course, String email) {
+        this.name = name;
+        this.id = id;
+        this.course = course;
+        this.email = email;
+    }
+
+}
